4fb60ec8b369c5a7bfdc1f27078eb45124d33f18
Diego Benavides <134892736+Diego-A-B@users.noreply.github.com>
2024-10-09 09:18:04 -0700

    Basic IO with cpp

---
+++ Basic IO with cpp.cpp
@@ -0,0 +1,26 @@
+/* Currently untitled C++ file*/
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main() 
+{
+
+    int num1;
+    int m,d,y;
+    cout<< "enter any integer"<<endl;
+    cin >> num1;
+    cout<< "thanks you are number 66"<< num1 <<endl ;
+
+    cout << "enter your bday"<< endl;
+    cin >>  m;
+    cin >> d;
+    cin >> y;
+    cout << "you entered your bday as:\n"<< m <<" "<< d << " " << y << endl;   
+
+
+    return 0;
+
+}
