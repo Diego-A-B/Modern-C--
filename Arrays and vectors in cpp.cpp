aa9f751571051d2a7881e41f5d9c519274e22ef6
Diego Benavides <134892736+Diego-A-B@users.noreply.github.com>
2024-10-09 10:25:25 -0700

    1-D array iteration

---
+++ Arrays and vectors in cpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using  std::cout;
+using std::endl;
+using std::cin;
+
+int main(){
+
+    int score [10] {100,100,85,70,90,90,95,65,77,87};
+
+    int i {0};
+
+    for(i;i<10;i++){
+
+        cout<< score[i]<< endl;
+
+    }
+
+    return 0;
+
+}
+
+
